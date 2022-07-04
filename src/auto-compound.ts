1e79463b98f8d41eea7695e561eaf77672414658
Idan Atar <idanatar@MacBook-Pro-5.018.co.il>
2022-07-04 15:00:42 +0300

    pos-analytics-lib 0.9.2

--- src/auto-compound.ts
+++ src/auto-compound.ts
@@ -30,6 +30,7 @@ async function getDelegatorsList() {
     let stakers: string[] = [];
     const allGuardians = await getGuardians(constants.nodeEndpoints)
     for (const guardian of allGuardians) {
+        console.log(`Working on guardian ${guardian}`)
         const g_info = await getGuardian(guardian.address, getWeb3());
         stakers.push(g_info.address);
         for (const d of g_info.delegators) {
